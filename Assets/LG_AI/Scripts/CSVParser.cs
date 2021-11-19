1e3d76b1ea91d9f0f28c166a62f3ca15f95baa73
JungHo Park <haplane5@gmail.com>
2021-11-19 14:57:46 +0900

    블랜드쉡입 애니메이션 적용

--- Assets/LG_AI/Scripts/CSVParser.cs
+++ Assets/LG_AI/Scripts/CSVParser.cs
@@ -6,19 +6,46 @@ using UnityEngine;
 public class CSVParser : MonoBehaviour
 {
     public SkinnedMeshRenderer skinnedMesh;
+    public AudioSource audioSource;
+    public Animator animator;
 
     List<Dictionary<string, object>> list = new List<Dictionary<string, object>>();
     private void Start()
     {
         list = CSVReader.Read("blendweights_Seq01");
+        Debug.Log(list.Count);
+    }
 
+    public void OnPlayButtonClick()
+    {
+        StartCoroutine(PlayBlendShape(0.15f));
+        StartCoroutine(PlaySound(0f));
+    }
+
+    IEnumerator PlayBlendShape(float _delayTime)
+    {
+        yield return new WaitForSeconds(_delayTime);
+        animator.SetBool("Speech", true);
         foreach (var l in list)
         {
             foreach (var _l in l)
             {
-                Debug.Log($"{_l.Key} : {_l.Value}");
+                var _idx = skinnedMesh.sharedMesh.GetBlendShapeIndex($"FACS_BSN.{_l.Key}");
+                skinnedMesh.SetBlendShapeWeight(_idx, float.Parse(_l.Value.ToString()) * 100);
             }
-            Debug.Log("");
+            yield return new WaitForFixedUpdate();
         }
+
+        animator.SetBool("Speech", false);
+        for (int i = 0; i < skinnedMesh.sharedMesh.blendShapeCount; i++)
+        {
+            skinnedMesh.SetBlendShapeWeight(i, 0);
+        }
+    }
+
+    IEnumerator PlaySound(float _delayTime)
+    {
+        yield return new WaitForSeconds(_delayTime);
+        audioSource.Play();
     }
 }
